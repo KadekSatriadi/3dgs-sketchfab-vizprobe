11bf231a696684b8e75768151333c69d8d94f390
Kadek Satriadi <ananta.satriadi@gmail.com>
2025-06-19 21:05:06 +1000

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+  
   base: '/3dgs-sketchfab-compare/',
 })
