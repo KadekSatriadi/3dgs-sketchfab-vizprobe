78eb54f02eee26e583ac500eb701819e614d5141
Kadek Satriadi <ananta.satriadi@gmail.com>
2025-06-24 16:33:58 +1000

    removed plugin

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Enable SharedArrayBuffer
-if (typeof SharedArrayBuffer === 'undefined') {
-  import('coi-serviceworker').then(({ register }) => {
-    register();
-  });
-}
+// if (typeof SharedArrayBuffer === 'undefined') {
+//   import('coi-serviceworker').then(({ register }) => {
+//     register();
+//   });
+// }
 
 createApp(App).mount('#app')
